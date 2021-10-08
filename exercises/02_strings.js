97be0583b092290f07bf6beba7b6fb19ecf4ef6c
luisnannini <luisnannini97@gmail.com>
2021-10-08 16:26:58 -0300

    added .js files with their excercises as comments and linked them to a new .html file

---
+++ exercises/02_strings.js
@@ -0,0 +1,22 @@
+//2) Strings
+
+/*a)Crear una variable de tipo string con al menos 10 caracteres y convertir todo el texto en mayúscula (utilizar
+toUpperCase).*/
+
+/*b)Crear una variable de tipo string con al menos 10 caracteres y generar un nuevo string con los primeros 5 
+caracteres guardando el resultado en una nueva variable (utilizar substring).*/
+
+/*c)Crear una variable de tipo string con al menos 10 caracteres y generar un nuevo string con los últimos 
+3 caracteres guardando el resultado en una nueva variable (utilizar substring).*/
+
+/*d)Crear una variable de tipo string con al menos 10 caracteres y generar un nuevo string con la primera 
+letra en mayúscula y las demás en minúscula. Guardar el resultado en una nueva variable (utilizar substring, 
+toUpperCase, toLowerCase y el operador +).*/
+
+/*e)Crear una variable de tipo string con al menos 10 caracteres y algún espacio en blanco. Encontrar la posición 
+del primer espacio en blanco y guardarla en una variable (utilizar indexOf).*/
+
+/*f)Crear una variable de tipo string con al menos 2 palabras largas (10 caracteres y algún espacio entre medio). 
+Utilizar los métodos de los ejercicios anteriores para generar un nuevo string que tenga la primera letra de ambas 
+palabras en mayúscula y las demás letras en minúscula (utilizar indexOf, substring, toUpperCase, toLowerCase y el 
+operador +).*/
---
+++ exercises/04_if_else.js
@@ -0,0 +1,19 @@
+//4) If Else
+
+/*a) Crear un número aleatorio entre 0 y 1 utilizando la función Math.random(), si el valor es mayor o igual que 0,5 
+mostrar una alerta con el mensaje “Greater than 0,5” y sino un alerta con el mensaje “Lower than 0,5”.*/
+
+/*b) Crear una variable “Age” que contenga un número entero entre 0 y 100 y muestre los siguientes mensajes de alerta:*/
+    /*I) “Bebe” si la edad es menor a 2 años;*/
+
+    /*II) “Nino” si la edad es entre 2 y 12 años;*/
+
+    /*III) “Adolescente” entre 13 y 19 años;*/
+
+    /*IV) “Joven” entre 20 y 30 años;*/
+
+    /*V) “Adulto” entre 31 y 60 años;*/
+
+    /*VI) “Adulto mayor” entre 61 y 75 años;*/
+
+    /*VII) “Anciano” si es mayor a 75 años.*/
